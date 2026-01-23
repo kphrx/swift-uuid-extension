6f45b8454542d7d49eb8d8fc472759dddf9ed77e
kPherox <kphrx@kpherox.dev>
2026-01-24 01:35:55 +0900

    feat: add UUIDv4 variant name

---
+++ Sources/UUIDExtension/UUID+v4.swift
@@ -0,0 +1,25 @@
+extension UUID {
+  public static func v4() -> Self {
+    .init()
+  }
+
+  public struct V4: UUIDVariantBase {
+    public var rawValue: UUID
+
+    public var version: UUID.Version {
+      .v4
+    }
+
+    public init() {
+      self.rawValue = .v4()
+    }
+
+    public init?(rawValue: UUID) {
+      guard case .v4 = rawValue.version else { return nil }
+      self.rawValue = rawValue
+    }
+  }
+}
+
+@available(iOS 17.0, macOS 14.0, tvOS 17.0, watchOS 10.0, *)
+extension UUID.V4: UUIDVariant {}
